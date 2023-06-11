a768283ac85a4bd9c24e103a9374e7a6f315147d
Kenneth <14003738+kennethjwong@users.noreply.github.com>
2023-06-11 14:14:36 -0700

    change port #

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const forecast = require("./utils/forecast");
 // console.log(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 30000;
 
 // define paths for Express config
 const publicPath = path.join(__dirname, "../public");
